68fb75f85d0084c178ce51b339d55d588a94df64
DanilSpizhevoi <spizhevyi_d@365.dnu.edu.ua>
2024-10-21 21:31:52 +0300

    е

--- laba 1/obj/Debug/net8.0-windows/laba 1.AssemblyInfo.cs
+++ laba 1/obj/Debug/net8.0-windows/laba 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5acef30e939f7534cd577e155c0d0d9609661c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528f866ade889e9dae8b2886493b9453a53be6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
